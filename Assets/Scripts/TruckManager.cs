bf9eddbe4dc6c35ffce8a4d515ebe7121c8892c5
Ellen <eol50559@gmail.com>
2021-12-02 00:12:18 +0000

    truck manager in place
    need to make contents random

--- Assets/Scripts/TruckManager.cs
+++ Assets/Scripts/TruckManager.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class TruckManager : MonoBehaviour
 {
-    public GameObject[] truck;
+    bool timerActivate;
+    int timeLeft;
+    public GameObject truck;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timerActivate = false;
+        timeLeft = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timerActivate){
+            timeLeft--;
+            if(timeLeft==0){
+                // print("rest")
+                Instantiate(truck, truck.transform.position, truck.transform.rotation);
+            }
+        }
     }
 
+    public void startTimer(){
+        timeLeft = Random.Range(10,200);
+        timerActivate = true;
+        print(timeLeft);
+    }
     // void OnTriggerEnter(Collider other) {
     //     if(other.gameObject.CompareTag("truck")){
     //         print("Stop truck");
--- Assets/Scripts/TruckMovement.cs
+++ Assets/Scripts/TruckMovement.cs
@@ -7,11 +7,17 @@ public class TruckMovement : MonoBehaviour
 
     bool stop = false;
     public GameObject uiScreen;
+    public GameObject mainUIScreen;
+    TruckManager manager;
     // Start is called before the first frame update
     private WorkerManager workerManager;
     void Start()
     {
         workerManager = FindObjectOfType<WorkerManager>();
+        manager = FindObjectOfType<TruckManager>();
+        uiScreen = GameObject.Find("Canvas").transform.Find("Sorting Panel").gameObject;
+        // uiScreen = mainUIScreen.Find;
+        uiScreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,8 +26,9 @@ public class TruckMovement : MonoBehaviour
         if(!stop){
             this.transform.Translate (Vector3.forward * 1 * Time.deltaTime);
         }
-
+       
         if(transform.position.z >5){
+            manager.startTimer();
             Destroy(this.gameObject);
         }
     }
