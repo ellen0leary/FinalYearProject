ffc43a1fe0bf3d8165865490433abaeb35f0513a
Ellen <eol50559@gmail.com>
2021-12-08 13:46:22 +0000

    random material now showing on ui

--- Assets/Scripts/TruckMaterial.cs
+++ Assets/Scripts/TruckMaterial.cs
@@ -11,12 +11,8 @@ public class TruckMaterial : MonoBehaviour
     //eventually import data from db
     //now hardcoded
     public Button btn;
-    public GameObject uiElement;
-    public GameObject material1;
-    public GameObject material2;
-    public GameObject heading;
-    public GameObject buttonText;
-
+    
+    public UiManager uiManager;
     TextMeshProUGUI title;
     
 
@@ -24,9 +20,7 @@ public class TruckMaterial : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        material1 = GameObject.Find("Material1");
-        material2 = GameObject.Find("Material2");
-         title = heading.GetComponent<TextMeshProUGUI>();
+        
     }
 
     // Update is called once per frame
@@ -37,13 +31,16 @@ public class TruckMaterial : MonoBehaviour
 
 
     public void setMaterial(){
+        uiManager = FindObjectOfType<UiManager>();
         int randInt1 = Random.Range(0, materials.Length);
         int randInt2 = Random.Range(0, materials.Length);
         
         
         
         // string materialName = materialNames[randInt1];
-        title.text =materials[randInt1];
+        uiManager.SetMaterialOne(materials[randInt1]);
+        uiManager.SetMaterialTwo(materials[randInt2]);
+        
         // TextMeshPro buttonText = uiElement.GetComponentInChildren<Button>().GetComponentInChildren<TextMeshPro>();
 
     }
--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
 
     public GameObject truck;
     TruckMovement script;
+    public GameObject material1Heading;
+    public GameObject material2Heading;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,14 @@ public class UiManager : MonoBehaviour
         
     }
 
+    public void SetMaterialOne(string name){
+        TextMeshProUGUI title = material1Heading.GetComponent<TextMeshProUGUI>();
+        title.text = name;
+    }
+    public void SetMaterialTwo(string name){
+        TextMeshProUGUI title = material2Heading.GetComponent<TextMeshProUGUI>();
+        title.text = name;
+    }
     public void sendWorkerPlastic(){
         print("Sending worker to get plastic");
         script.closeScreen();
