934fc7e506e9b4df69f740c4022fffb2160a6d6d
Ellen <eol50559@gmail.com>
2022-01-14 19:06:27 +0000

    Random numver of trucks

--- Assets/Scripts/TruckManager.cs
+++ Assets/Scripts/TruckManager.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TruckManager : MonoBehaviour
 {
     bool timerActivate;
     int timeLeft;
     public GameObject truck;
+    int numOfTrucks;
     // Start is called before the first frame update
     void Start()
     {
         timerActivate = false;
         timeLeft = 0;
+        numOfTrucks = Random.Range(0, 6);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timerActivate){
+        if(numOfTrucks<0){
+            SceneManager.LoadScene(2);
+        }else if(timerActivate){
             timeLeft--;
             if(timeLeft==0){
                 // print("rest")
@@ -29,7 +34,7 @@ public class TruckManager : MonoBehaviour
     public void startTimer(){
         timeLeft = Random.Range(10,200);
         timerActivate = true;
-        print(timeLeft);
+        numOfTrucks--;
     }
     // void OnTriggerEnter(Collider other) {
     //     if(other.gameObject.CompareTag("truck")){
