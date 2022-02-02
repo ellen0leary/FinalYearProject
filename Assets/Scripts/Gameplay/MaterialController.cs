ac397bc5a1d20813e77b577449616a62129808f2
Ellen <eol50559@gmail.com>
2022-02-02 20:42:33 +0000

    scoring pt2
    wait for pt 3

--- Assets/Scripts/Gameplay/MaterialController.cs
+++ Assets/Scripts/Gameplay/MaterialController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MaterialController : MonoBehaviour
 {
-    int[] material = {0,1,2,3};
+    int currentScore = 0;
+    int[] material = {0,1,2};
     int value = 1000;
-    int cuurentIndex = 0;
-    int[] currentProcess = {-1-1,-1,-1};
+    int curentIndex = 0;
+    int[] currentProcess = {-1-1,-1};
     int extraActons = 0;
     // Start is called before the first frame update
     void Start()
@@ -25,9 +26,36 @@ public class MaterialController : MonoBehaviour
     public void materialFinished(int layer){
         //add to []
         Debug.Log(layer);
+        
     }
 
     public void sell(){
         //calculate
     }
+
+
+
+
+    /** 
+    base class for materials
+    **/
+    public class Material {
+        enum Order {
+            NEW = 8,
+            WASH,
+            SORT,
+            SHREAD,
+            DONE
+        }
+
+        Order loc;
+        int error;
+
+        public Material(){
+            error = 0;
+            loc = Order.NEW;
+        }
+    }
 }
+
+//create base class with enum to track 
