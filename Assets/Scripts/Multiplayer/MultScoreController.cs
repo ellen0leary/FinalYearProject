e08e9d6dc1dd54026594adca3e398f0cea5a9184
Ellen <eol50559@gmail.com>
2022-02-24 11:32:02 +0000

    multi score controller created

---
+++ Assets/Scripts/Multiplayer/MultScoreController.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class MultScoreController : MonoBehaviour
+{
+    int startingScore = 5000;
+    int scoreP1;
+    int scoreP2;
+    // Start is called before the first frame update
+ // player 1
+    public TextMeshProUGUI scoreTextP1;
+    public TextMeshProUGUI countTextP1;
+    GameObject scorePanelP1;
+    int materCountP1 = 0;
+
+    // player 2
+    public TextMeshProUGUI scoreTextP2;
+    public TextMeshProUGUI countTextP2;
+    GameObject scorePanelP2;
+    int materCountP2 = 0;
+
+
+
+    TextMeshProUGUI timerTxt;
+    float mainTimer = 1000f;
+    int materGoal = 20;
+    GameObject scorePanel;
+    void Start()
+    {
+        scoreP1 = startingScore;
+        scoreP2 = startingScore;
+        scorePanelP1 = GameObject.Find("Score Panel");
+        scorePanelP1.SetActive(false);
+        scorePanelP2 = GameObject.Find("Score Panel");
+        scorePanelP2.SetActive(false);
+
+        timerTxt = GameObject.Find("TimerText").GetComponent<TextMeshProUGUI>();
+
+        countTextP1.text = "Material Count " + materCountP1.ToString() + "/" + materGoal.ToString();
+        countTextP2.text= "Material Count " + materCountP2.ToString() + "/" + materGoal.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainTimer -= 1 * Time.deltaTime;
+        if(mainTimer<=0){
+            gameOverScreen();
+        } else {
+            timerTxt.text =  "Timer Remaining : " + Mathf.FloorToInt(mainTimer % 60);
+            countTextP1.text = "Material Count " + materCountP1.ToString() + "/" + materGoal.ToString();
+            countTextP2.text = "Material Count " + materCountP2.ToString() + "/" + materGoal.ToString();
+        }
+    }
+
+    public bool setP1Score(int addition, bool increseCounter){
+        if(scoreP1 + addition >=0){
+            scoreP1 += addition;
+            setScoreText();
+            if(increseCounter) materCountP1++;
+            return true;
+        }
+        return false;
+    }
+
+    public bool setP2Score(int addition, bool increseCounter)
+    {
+        if(scoreP2 + addition >=0){
+            scoreP2 += addition;
+            setScoreText();
+            if(increseCounter) materCountP1++;
+            return true;
+        }
+        return false;
+    }
+    void setScoreText(){
+        scoreTextP1.text = "Money - €" + scoreP1.ToString();
+        scoreTextP2.text = "Money - €" + scoreP2.ToString();
+    }
+    void gameOverScreen(){
+        string p1EndText = "";
+        string p2EndText = "";
+        if(scoreP1 > scoreP2){
+            p1EndText = "You Won!";
+            p2EndText = "You Lost.....";
+        }else  {
+            p2EndText = "You Won!";
+            p1EndText = "You Lost.....";
+        }
+        scorePanel.SetActive(true);
+        GameObject.Find("FinalTextScoreP1").GetComponent<TextMeshProUGUI>().text = p1EndText;
+        //set final score
+        GameObject.Find("FinalScoreScoreP1").GetComponent<TextMeshProUGUI>().text = "Final Score is " + scoreP1.ToString();
+        GameObject.Find("FinalTextScoreP2").GetComponent<TextMeshProUGUI>().text = p2EndText;
+        //set final score
+        GameObject.Find("FinalScoreScoreP2").GetComponent<TextMeshProUGUI>().text = "Final Score is " + scoreP2.ToString();
+    }
+    public void goToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    void goToReplay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void goToQuiz()
+    {
+        SceneManager.LoadScene("Quiz");
+    }
+}
