b2c27758dd7638e4ad5d62422972a4a8f091d588
Ellen <eol50559@gmail.com>
2022-02-26 15:01:34 +0000

    instruction screen made

---
+++ Assets/InstructionScreenController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionScreenController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame(){
+        this.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
